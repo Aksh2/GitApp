ed73c47f99301b8e7839879ef91c238587ee664b
Akshay Kumar H <akshaykumarh94@gmail.com>
2025-04-16 01:04:07 +0530

    Added chip component.

--- app/src/main/java/com/project/ui/baseui/BaseComponents.kt
+++ app/src/main/java/com/project/ui/baseui/BaseComponents.kt
@@ -4,11 +4,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 
 @Composable
 fun ErrorOverlay(errorMessage: String) {
@@ -24,5 +29,19 @@ fun Loader(){
             CircularProgressIndicator()
         }
     }
+}
 
+@Composable
+fun ChipText(text: String, icon: ImageVector){
+    AssistChip(
+        onClick = {},
+        label = { Text(text) },
+        leadingIcon = {
+            Icon(
+                icon,
+                contentDescription = "Localized description",
+                Modifier.size(AssistChipDefaults.IconSize)
+            )
+        }
+    )
 }
