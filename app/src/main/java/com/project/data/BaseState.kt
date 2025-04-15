1da6e670fd0cb73dd10c7c884d579630e703ab56
Akshay Kumar H <akshaykumarh94@gmail.com>
2025-04-15 18:18:22 +0530

    Added base state.

--- app/src/main/java/com/project/data/BaseState.kt
+++ app/src/main/java/com/project/data/BaseState.kt
@@ -2,6 +2,6 @@ package com.project.data
 
 sealed class BaseState<T> {
     class Loading<T> : BaseState<T>()
-    data class Success<T>(val users: T) : BaseState<T>()
+    data class Success<T>(val response: T) : BaseState<T>()
     data class Error<T>(val message: String) : BaseState<T>()
 }
